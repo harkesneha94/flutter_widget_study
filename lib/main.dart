92baea74a9dcd83f0fe2f21a5f618c9319342b12
Sneha Harke <sneha.harke@ril.com>
2020-05-11 10:00:50 +0530

    Change crossAxisAlignments with an invisible container

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,26 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            // try to align at right
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
               Container(
                 height: 100,
-                width: 200,
                 color : Colors.white,
                 child: Text('Container 1'),
               ),
               Container(
                 height: 100,
-                width: 100,
                 color : Colors.blue,
                 child: Text('Container 2'),
               ),
               Container(
                 height: 100,
-                width: 100,
                 color : Colors.orange,
                 child: Text('Container 3'),
+              ),
+              Container(
+                width: double.infinity,
               )
           ],
           ),
