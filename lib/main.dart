3f840dbf88d7caf51d971c2a841c6dcb24df2324
Sneha Harke <sneha.harke@ril.com>
2020-05-11 09:47:22 +0530

    Chnage mainAxisSize

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisSize: MainAxisSize.min, 
             children: <Widget>[
               Container(
                 height: 100,
